023ff9422a806bb6ca4561c4cc33b275384d2cd4
malornh <jivko.ivanov14@gmail.com>
2024-07-31 18:49:54 +0300

    Add ReservationMadeBySameUser Unit Tests

--- TestNinja.UnitTests/ReservationTests.cs.cs
+++ TestNinja.UnitTests/ReservationTests.cs.cs
@@ -20,5 +20,46 @@ namespace TestNinja.UnitTests
             Assert.IsTrue(result);
 
         }
+
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsNotAdmin_ShouldReturnFalse()
+        {
+            // Arrange
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = false });
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserWhoMadeReservation_ShouldReturnTrue()
+        {
+            // Arrange
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            // Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_NotSameUserWhoMadeReservation_ShouldReturnFalse()
+        {
+            // Arrange
+            var reservation = new Reservation { MadeBy = new User() };
+
+            // Act
+            var result = reservation.CanBeCancelledBy(new User());
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
     }
 }
