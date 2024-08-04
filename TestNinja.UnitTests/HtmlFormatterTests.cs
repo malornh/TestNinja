9f0a6117bd40456c652721172bc867121ec2aad9
malornh <jivko.ivanov14@gmail.com>
2024-08-04 19:00:33 +0300

    Testing a string

---
+++ TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    internal class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abc");
+
+            Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            Assert.That(result, Does.EndWith("</strong>").IgnoreCase);
+            Assert.That(result, Does.Contain("abc").IgnoreCase);
+        }
+    }
+}
