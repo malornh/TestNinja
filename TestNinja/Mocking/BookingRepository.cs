a714084a0ad2e1da947e91831dfda006eac004da
malornh <jivko.ivanov14@gmail.com>
2024-08-13 21:14:50 +0300

    Refactor Booking Helper, Extract IBookingRepository Writing Tests for BookingHelper

---
+++ TestNinja/Mocking/BookingRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Mocking
+{
+    public interface IBookingRepository
+    {
+        IQueryable<Booking> GetActiveBookings(int? excludableBookingId = null);
+    }
+
+    public class BookingRepository : IBookingRepository
+    {
+        public IQueryable<Booking> GetActiveBookings(int? excludableBookingId = null)
+        {
+            var unitOfWork = new UnitOfWork();
+            var bookings =
+                unitOfWork.Query<Booking>()
+                .Where(
+                    b => b.Status != "Cancelled");
+
+            if (excludableBookingId.HasValue)
+                bookings = bookings.Where(b => b.Id != excludableBookingId.Value);
+
+            return bookings;
+        }
+    }
+}
